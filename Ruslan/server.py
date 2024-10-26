0795d1d310e0a4e028ad5013f4dafb1c27545872
Ruslan <ruslan.yarmak.intech@gmail.com>
2024-10-26 17:58:15 +0300

    update 0.7

--- Ruslan/server.py
+++ Ruslan/server.py
@@ -1,11 +1,13 @@
 import pika
 import json
+import time
 
 class AddServer:
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))  # Подключение.
     channel = connection.channel()  # Создание канала.
 
     def __init__(self):
+        self.active_informants = [] #
         self.channel.queue_declare(queue='server', durable=True)
         self.channel.basic_qos(prefetch_count=1)
         self.run_consuming()
@@ -18,17 +20,27 @@ class AddServer:
 
         # Проверка активных информаторов.
         if agent.startswith('customer') and operation == 'available informant':
-            self.get_active_informants()
+            print('-- Collecting data')
+            self.get_active_informants()  # Сбор активных информаторов.
+            time.sleep(3)
+            if self.active_informants:
+                informant = self.active_informants[0]
+            else:
+                informant = 'no active informants'
+            response = {'queue': 'server', 'operation': 'available informant', 'text': informant}
+            channel.basic_publish(
+                exchange='',
+                routing_key=agent,
+                body=json.dumps(response)
+            )
+            self.active_informants = []  # Обнуление списка активных информаторов.
+
 
         # Подтверждение активности от информатора.
         elif agent.startswith('informant') and operation == 'activity confirmation':
             print(f'-- {agent} active')
-            # data = {'queue': 'server', 'operation': 'available informant', 'text': 'informant1'}
-            # channel.basic_publish(
-            #     exchange='',
-            #     routing_key=agent,
-            #     body=json.dumps(data),  # Отправляет заказчику свободного информатора.
-            # )
+            self.active_informants.append(agent)
+
 
     def run_consuming(self):
         """Начинает принимать сообщения."""

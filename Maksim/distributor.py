dd7c6e70e92fb110ec2d6c63176797d8a71e06f1
Maksim <maksim.malhinau@gmail.com>
2024-10-29 15:09:58 +0300

    informant v.0.2

--- Maksim/distributor.py
+++ Maksim/distributor.py
@@ -1,57 +1,52 @@
 import pika
 import json
 import time
+import threading
 
 class Distributor:
     def __init__(self, distributor_name):
         self.distributor_name = distributor_name
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        self.channel = self.connection.channel()
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = connection.channel()
         
         self.channel.queue_declare(queue=self.distributor_name, durable=True)
+        self.channel.exchange_declare(exchange='all_agents', exchange_type='fanout', durable=True)
+        self.channel.queue_bind(exchange='all_agents', queue=self.distributor_name)
 
-        self.run_consuming()
+        t1 = threading.Thread(target=self.run_consuming)
+        t1.start()
 
     def callback(self, channel, method, properties, body):
         data = json.loads(body)
-        print(data)
+        agent = data['queue']
         operation = data['operation']
-        
-        # Обработка запроса на задачу
-        if operation == 'assign task':
-            task_duration = data.get('task_duration', 0)
-            print(f"Received task with duration {task_duration} seconds.")
-            
-            # Разделение задачи между исполнителями(тоже пока заглушка)
-            num_executors = 3
-            part_duration = task_duration / num_executors
-            
-            # Заглушка для распределения задач между исполнителями
-            for i in range(1, num_executors + 1):
-                executor_name = f'executor{i}'
-                self.send_task_to_executor(executor_name, part_duration)
 
-            # Отправка отчета информатору после завершения задачи
-            report = {'queue': 'informant', 'operation': 'task completed', 'distributor': self.distributor_name}
+        if operation == 'submit_task':
+            task_duration = data.get('text', 0)
+            print(f"Received task with duration {task_duration} seconds.")
+            self.execute_task(task_duration)
+
+        elif operation == 'activity check':
+            response = {
+                'queue': self.distributor_name,
+                'operation': 'activity confirmation',
+                'text': 'ACTIVE'
+            }
             self.channel.basic_publish(
                 exchange='',
-                routing_key='info1',
-                body=json.dumps(report)
+                routing_key=agent,
+                body=json.dumps(response)
             )
-            print(f"Task completed and reported to informant.")
 
-    def send_task_to_executor(self, executor_name, duration):
-        # Заглушка для отправки задачи исполнителю
-        print(f"Sending {duration} seconds of task to {executor_name}")
+    def execute_task(self, duration):
+        print(f"{self.distributor_name} is executing task for {duration} seconds.")
         time.sleep(duration)
+        print(f"{self.distributor_name} completed the task.")
 
     def run_consuming(self):
-        self.channel.basic_consume(
-            queue=self.distributor_name,
-            auto_ack=True,
-            on_message_callback=self.callback
-        )
-        print(f"{self.distributor_name} started listening for tasks.")
+        self.channel.basic_consume(queue=self.distributor_name, auto_ack=True, on_message_callback=self.callback)
+        print(f"{self.distributor_name} started listening for tasks and activity checks.")
         self.channel.start_consuming()
 
 Distributor('distributor1')
--- Maksim/informant.py
+++ Maksim/informant.py
@@ -1,47 +1,69 @@
 import pika
 import json
+import time
+import threading
 
 class Informant:
     def __init__(self, informant_name):
         self.informant_name = informant_name
+        self.active_allocators = []
 
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        self.channel = self.connection.channel()
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        connection2 = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = connection.channel()
+        self.channel2 = connection2.channel()
+
         self.channel.exchange_declare(exchange='all_agents', exchange_type='fanout', durable=True)
+        self.channel2.exchange_declare(exchange='all_agents', exchange_type='fanout', durable=True)
         self.channel.queue_declare(queue=self.informant_name, durable=True)
         self.channel.queue_bind(exchange='all_agents', queue=self.informant_name)
 
-        self.run_consuming()
+        t1 = threading.Thread(target=self.collect_allocators)
+        t1.start()
+        t2 = threading.Thread(target=self.run_consuming)
+        t2.start()
 
     def callback(self, channel, method, properties, body):
         data = json.loads(body)
         agent = data['queue']
         operation = data['operation']
-        # в поле text заглушка
-        if operation == 'available allocator':
-            response = {'queue': self.informant_name, 'operation': 'available allocator', 'text': 'distributor1'}
-            self.channel.basic_publish(
-                exchange='',
-                routing_key=agent,
-                body=json.dumps(response)
-            )
+        
+
+        if agent == 'server' and operation == 'activity check':
+            response = {'queue': self.informant_name, 'operation': 'activity confirmation', 'text': 'ONLINE'}
+            self.channel.basic_publish(exchange='', routing_key='server', body=json.dumps(response))
+
+        elif operation == 'available allocator':
+            if self.active_allocators:
+                allocator = self.active_allocators[0]
+                allocator = 'no active allocators'
+            response = {
+                'queue': self.informant_name,
+                'operation': 'available allocator',
+                'text': allocator
+            }
+            self.channel.basic_publish(exchange='', routing_key=agent, body=json.dumps(response))
             print(f"Sent available allocators to {data['queue']}")
 
-        # Отправка подтверждения серверу статуса активности.
-        elif agent == 'server' and operation == 'activity check':
-            response = {'queue': self.informant_name, 'operation': 'activity confirmation', 'text': 'ONLINE'}
-            self.channel.basic_publish(
-                exchange='',
-                routing_key='server',
-                body=json.dumps(response)
-            )
+        elif operation == 'activity confirmation':
+            if agent not in self.active_allocators:
+                self.active_allocators.append(agent)
+                print(f"Added {agent} to active allocators.")
+
+    def collect_allocators(self):
+        while True:
+            self.active_allocators = []
+            data = {'queue': self.informant_name, 'operation': 'activity check', 'text': 'confirm activity status'}
+            self.channel2.basic_publish(
+                exchange='all_agents',
+                routing_key='',
+                body=json.dumps(data)
+                )
+            time.sleep(5)
+            print(f'-- Active allocators: {self.active_allocators}')
 
     def run_consuming(self):
-        self.channel.basic_consume(
-            queue=self.informant_name,
-            auto_ack=True,
-            on_message_callback=self.callback
-        )
+        self.channel.basic_consume(queue=self.informant_name, auto_ack=True, on_message_callback=self.callback)
         print(f"{self.informant_name} started listening for requests.")
         self.channel.start_consuming()
 

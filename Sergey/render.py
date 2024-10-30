b52b4c83bc90017e52e017730dd1484937a8db2a
Ruslan <ruslan.yarmak.intech@gmail.com>
2024-10-30 12:31:17 +0300

    update render 0.11

--- Sergey/render.py
+++ Sergey/render.py
@@ -1,3 +1,4 @@
+from http.client import responses
 from pickle import REDUCE
 
 import pika
@@ -6,37 +7,49 @@ import json
 
 class Render:
 
-    def __init__(self,queue_name,render_name,host='localhost'):
-        self.render_name = render_name
-        self.queue_name = queue_name
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+    def __init__(self,render_name):
+        self.queue_name = render_name
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         self.channel = self.connection.channel()
+
         self.channel.queue_declare(queue=self.queue_name, durable=True)
+        self.channel.exchange_declare(exchange='all_agents', exchange_type='fanout', durable=True)
         self.channel.basic_qos(prefetch_count=1)
+        self.channel.queue_bind(exchange='all_agents', queue=self.queue_name)
 
+        self.start_consume()
 
-
-    def connect(self):
-        self.channel.basic_consume(queue=self.queue_name,
-                                   on_message_callback=self.callback)
 
     def callback(self, ch, method, properties, body):
+        print(body)
         data = json.loads(body)
-        self.channel.basic_publish(exchange='',
-                                   routing_key=self.queue_name,
-                                   body=f'{self.render_name} is connected.')
-        print(f'{self.render_name} received task : ...')
-        time.sleep(data.get('duration'))
-        result = f'Result is ...'
-        self.channel.basic_publish(exchange='',
-                                   routing_key=self.queue_name,
-                                   body=json.dumps({'task':'completed',
-                                                    'result':result}))
-        ch.basic_ack(deliver_tag=method.delivery_tag)
+        agent = data['queue']
+        operation = data['operation']
+        text = data['text']
+
+        if operation == 'activity check':
+            #ch.basic_ack(deliver_tag=method.delivery_tag)
+            response = {'queue': self.queue_name, 'operation': 'activity confirmation', 'text': 'ACTIVE'}
+            self.channel.basic_publish(
+                exchange='',
+                routing_key=agent,
+                body=json.dumps(response)
+            )
+            #ch.basic_ack(deliver_tag=method.delivery_tag)
+        elif operation == 'execute_task':
+            for i in range(int(text)):
+                print(i)
+            time.sleep(1)
+            response = {'queue': self.queue_name, 'operation': 'task completed', 'text': 'task completed'}
+            self.channel.basic_publish(exchange='',
+                                       routing_key=agent,
+                                       body=json.dumps(response))
+            #ch.basic_ack(deliver_tag=method.delivery_tag)
 
     def start_consume(self):
+        self.channel.basic_consume(queue=self.queue_name,
+                                   auto_ack=True,
+                                   on_message_callback=self.callback)
         self.channel.start_consuming()
 
-if __name__ == '__main__':
-    rend = Render('distributor','pc1')
-    rend.start_consume()
+Render('render1')
